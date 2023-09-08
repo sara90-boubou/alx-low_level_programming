ee815d9cf233ac6c16327a6df02389f171c43a84
sara90-boubou <saraboubou4@gmail.com>
2023-09-08 16:05:09 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-print("%i is positive\n", n);
+print("%d is positive\n", n);
 else if (n == 0)
-print("%i is zero\n", n);
+print("%d is zero\n", n);
 else
-print("%i is negative\n", n);
+print("%d is negative\n", n);
 return (0);
 }
