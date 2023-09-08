daec283a99e6a128e89ff3786dbb228f7d107be9
sara90-boubou <saraboubou4@gmail.com>
2023-09-07 19:35:52 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point
-  *
-  * Description: Print with puts
-  *
-  * Return: 0 (Success)
-  */
+ * main - Entry point
+ *
+ * Description: Print with puts
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
