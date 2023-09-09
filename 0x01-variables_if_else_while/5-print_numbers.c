5940ad87c8d2bf9337605bf059d9491c51bb19fa
sara90-boubou <saraboubou4@gmail.com>
2023-09-09 11:56:09 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Single digits of base 10.
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf(i);
+}
+printf('\n');
+return (0);
+}
