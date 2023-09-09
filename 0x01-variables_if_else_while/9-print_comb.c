c091edb1c204e44ea0ce28eff2eed00847daf6c0
sara90-boubou <saraboubou4@gmail.com>
2023-09-09 16:34:42 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Digits with comma and space.
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(i);
+putchar(',');
+puchar(' ');
+return (0);
+}
+
