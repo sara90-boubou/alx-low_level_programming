4388b94dc839ddfa8f011e8b90b5dc710d3fd49b
sara90-boubou <saraboubou4@gmail.com>
2023-09-09 16:13:24 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: digits and character with putchar.
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+char i;
+char j;
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+for (j = 'a'; j <= f; j++)
+putchar(j);
+putchar('\n');
+return (0);
+}
