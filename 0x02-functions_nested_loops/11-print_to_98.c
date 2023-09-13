ca1d14538227995ab7d3fc7e4ceff940801ac52e
sara90-boubou <saraboubou4@gmail.com>
2023-09-13 15:32:39 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,9 +12,9 @@ void print_to_98(int n)
 int count;
 if (n > 98)
 for (count = n; count > 98; count--)
-print("%d, ", count);
+printf("%d, ", count);
 else
 for (count = n; count < 98; count++)
-print("%d, ", count);
-print("98\n");
+printf("%d, ", count);
+printf("98\n");
 }
