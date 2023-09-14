a2aee45b87ada142003e4c9f16161d2b8862e15f
sara90-boubou <saraboubou4@gmail.com>
2023-09-14 16:46:52 -0700

    task1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point of program
+*
+* Description : Commented out the code causing the infinite loop
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+/*i = 0;*/
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
