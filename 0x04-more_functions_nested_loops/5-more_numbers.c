891b7ea67bc115e4e276b0d8832c1327ec83b885
sara90-boubou <saraboubou4@gmail.com>
2023-09-15 15:47:15 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,13 +4,15 @@
 */
 void more_numbers(void)
 {
+int i;
+int j;
 for (i = 0; i <= 14; i++)
 {
 for (j = 0; j < 10; j++)
 {
 _putchar(i + '0');
-_putchar(j + '0');
 }
 _putchar('\n');
 }
+_putchar('\n');
 }
