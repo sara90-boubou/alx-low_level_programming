8703afb3c09ce562161c64cf7ec5487d0c8f0ec4
sara90-boubou <saraboubou4@gmail.com>
2023-09-14 18:31:04 -0700

    task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
-* main - tests function that prints if integer is positive or negative
+* main - Entry point
+*
+* Description :tests function that prints if integer is positive or
+*   negative or null
+*
 * Return: 0
 */
 int main(void)
