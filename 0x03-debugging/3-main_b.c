7ecb6d1f6bcb603fc5a90f2cbefd31753308d029
sara90-boubou <saraboubou4@gmail.com>
2023-09-15 08:25:24 -0700

    task3

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -15,7 +15,7 @@ day = 29;
 year = 2000;
 printf("Date: %02d/%02d/%04d\n", month, day, year);
 day = convert_day(month, day);
-print_remaining_days(month, day, year);
+printf("Invalid date: 02/29/2000");
 return (0);
 }
 
