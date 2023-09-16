7cfa91ba2b093f67b230607f03554712fb35de14
sara90-boubou <saraboubou4@gmail.com>
2023-09-15 18:53:27 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* main - print 1 to 100 and fizzbuss
+*
+* Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 100; i++)
+{
+if ((i % 3) == 0 && (i % 5) == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("Buzz");
+printf("\n");
+return (0);
+}
+
