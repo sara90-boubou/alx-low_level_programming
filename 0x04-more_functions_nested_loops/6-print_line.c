a87ed6af6cba57d9dcf4d91250845ad88e9678fd
sara90-boubou <saraboubou4@gmail.com>
2023-09-15 17:07:28 -0700

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_line - Print a line of underscores.
+* @n: The number of underscores to print.
+*
+* If n is less than or equal to 0, it prints a newline character.
+*/
+void print_line(int n)
+{
+int i;
+if (n <= 0)
+_putchar('\n');
+else
+for (i = 1; i <= n; i++)
+_putchar('_');
+_putchar('\n');
+}
