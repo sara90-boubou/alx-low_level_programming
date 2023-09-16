de4863df34e78f07952f0b8e74076212447ace16
sara90-boubou <saraboubou4@gmail.com>
2023-09-15 17:21:42 -0700

    task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,12 @@
 void print_line(int n)
 {
 int i;
-if (n <= 0)
-_putchar('\n');
-else
-for (i = 1; i <= n; i++)
+for (i = 0; i < n; i++)
+{
+if (n > 0)
 {
 _putchar('_');
-_putchar('\n');
 }
+}
+_putchar('\n');
 }
