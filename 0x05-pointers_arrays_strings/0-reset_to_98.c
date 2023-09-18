81d8060e5723b281073a3c1beb88f60a6e8be8f1
sara90-boubou <saraboubou4@gmail.com>
2023-09-18 11:18:22 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* This function sets the value pointed to by the provided pointer to 98.
+*
+* @param n - A pointer to an integer whose value will be set to 98.
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
