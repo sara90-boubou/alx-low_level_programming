b7ec58a1f08bf7cf5714bfa350f43fc8013a59f2
sara90-boubou <saraboubou4@gmail.com>
2023-09-18 15:16:16 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* puts_half - Prints the second half of a null-terminated string.
+*
+* @str: A pointer to a null-terminated string.
+*
+* Description: This function takes a pointer to a null-terminated string and
+* prints the second half of the string.
+*/
+void puts_half(char *str)
+{
+int longi = 0;
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+return (longi);
+}
