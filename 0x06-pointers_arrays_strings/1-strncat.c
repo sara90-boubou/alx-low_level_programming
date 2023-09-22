642a4952b466d620195fe35dfb33c3f9e52c0916
sara90-boubou <saraboubou4@gmail.com>
2023-09-22 08:16:43 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,12 +16,13 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int c, i;
+c = 0;
 while (dest[c])
-	c++;
+c++;
 for (i = 0; i < n && src[i] != '\0'; i++)
 {
 dest[c + i] = src[i];
-dest[c + i] = '\0';
 }
+dest[c + i] = '\0';
 return (dest);
 }
