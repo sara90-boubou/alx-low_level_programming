aa98b85196ea76a7f3749fcc5b5fbe3e23c23d7a
sara90-boubou <saraboubou4@gmail.com>
2023-09-22 14:33:12 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,16 +15,11 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-int aqual = 0;
-while (*s1)
+int i;
+int b = 0;
+for (i = 0; s1[i] != '\0' && b == 0; i++)
 {
-if (*s1 != *s2)
-{
-equal = ((int)*s1 - 48) - ((int)*s2 - 48);
-break;
+b = s1[i] - s2[i];
 }
-s1++;
-s2++;
-}
-return (equal);
+return (b);
 }
