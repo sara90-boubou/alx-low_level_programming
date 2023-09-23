7153220f2ab52a65660ca4d0d8510f999ecf9e76
sara90-boubou <saraboubou4@gmail.com>
2023-09-22 17:03:33 -0700

    task7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* leet - Encode a string into "leet" (1337) speak.
+*
+* @str: The string to be encoded.
+*
+* Return: A pointer to the modified string.
+*/
+char *leet(char *str)
+{
+int i;
+int j;
+char c[] = "aAeEoOtTlL";
+char c[] = "4433007711";
+for (i = 0; c[i] != '\0'; i++)
+{
+for (j = 0; c[j] != '\0'; j++)
+{
+if (str[i] == c[j])
+{
+str[i] = d[j];
+}
+}
+}
+return (str);
+}
