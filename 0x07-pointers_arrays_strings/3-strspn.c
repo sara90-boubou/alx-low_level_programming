fc908de2854e2f626e6b41c85873f5344c18da1a
sara90-boubou <saraboubou4@gmail.com>
2023-09-25 16:10:19 -0700

    task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strspn - function name
+* @s: parameter 1
+* @accept: parameter 2
+* Return: characters counted
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+int i;
+int k;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (k = 0; s[i] != accept[k]; k++)
+{
+if (accept[k] == '\0')
+return (i);
+}
+}
+return (0);
+}
