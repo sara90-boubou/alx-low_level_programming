109fdd939bb130f0031643833dc3033bdc95bd1a
sara90-boubou <saraboubou4@gmail.com>
2023-10-02 18:39:56 -0700

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - multiples two numbers
+*
+* @argc: number of command line arguments.
+* @argv: array that contains the command line arguments.
+*
+* Return: (0) when succeeded
+*/
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
