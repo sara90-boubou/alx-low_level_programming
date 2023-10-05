8726f7c15a7f73249fb147933f5018cfb9c95ede
sara90-boubou <saraboubou4@gmail.com>
2023-10-05 14:08:57 -0700

    task4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* free_grid - ...
+* @grid: ...
+* @height: ...
+*
+* Return: ...
+*/
+void free_grid(int **grid, int height)
+{
+int i, j;
+for (i = 0; i < height; i++)
+{
+if (grid[i] == NULL)
+{
+for (j = i; j >= 0; j--)
+{
+free(grid[j]);
+}
+free(grid);
+}
+}
+}
